496bd3d551c19f305a36688fdd30ecb55c78f44c
Deniz Caygöz <deniz.caygoez@tu-dortmund.de>
2024-02-29 10:42:29 +0100

    Upload New File

---
+++ src/main/kotlin/entity/Player.kt
@@ -0,0 +1,28 @@
+package entity
+/**
+ *This class to represents a player in the game.
+ */
+class Player(val name: String) {
+    /**
+      *Indicates whether the player has performed a 'push' action in the current round.
+      */
+    var hasPushed: Boolean = false
+    /**
+     *Indicates whether the player has performed a 'swap' action in the current round.
+     */
+    var hasSwapped: Boolean = false
+    /**Represents the cards that are hidden from other players.
+     * These are the cards in the player's hand that only the player can see.
+     */
+    var hiddenCards: MutableList<Card> = mutableListOf()
+    /**
+     *Represents the cards that are visible to all players.
+     *These are the cards placed on the table in front of the player, face up.
+     */
+    var openCards: MutableList<Card> = mutableListOf()
+
+    /**
+     * handResult represents the hand value player has.
+     */
+    var handResult: PokerHand = PokerHand.NONE
+}
